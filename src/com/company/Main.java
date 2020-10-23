6a2e0e2bf0926298fef19d2eac25e1a914014e5a
O'Hara <24cohara@student.dasd.org>
2020-10-23 09:24:12 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// int P = 1; int K = 0.45359237; int P = K; int 5(P) =
+    }
+}
---
+++ src/com/company/Weighty.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Weighty {
+
+    public static void main(String[] args) {
+    }}
+
+
+
